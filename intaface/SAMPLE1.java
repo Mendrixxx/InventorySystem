c0c6e0390e275cc9f157ed658cdd8b3f3c40d3d7
KRcsa <karlorhommelchua.staana@bicol-u.edu.ph>
2021-07-06 13:07:21 +0800

    edits

--- intaface/SAMPLE1.java
+++ intaface/SAMPLE1.java
@@ -178,7 +178,7 @@ public class SAMPLE1 extends javax.swing.JFrame {
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(11, 11, 11)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
@@ -374,6 +374,8 @@ public class SAMPLE1 extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void deleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteActionPerformed
+        int index = jTable1.getSelectedRow();
+        System.out.println(index);
             
     }//GEN-LAST:event_deleteActionPerformed
 
