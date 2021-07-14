8d8a622a0f166aa51ca42f2ed98fb13b24464d5b
Jose David De Los Santos <esoj.divad025@gmail.com>
2021-07-14 14:28:35 +0800

    Change pass working?
    pa add nalang ng pag redirect kung saan man pag clinick ang cancel dito sa pag change pass, tapos pa lagay naman ng logout button sa kung saan man pag naka pasok na

---
+++ change_pass.php
@@ -0,0 +1,54 @@
+<?php
+//session_start();
+include 'backend/conn.php';
+
+if (isset($_POST['op']) && isset($_POST['np']) && isset($_POST['c_np'])) {
+  function validate($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $op = validate($_POST['op']);
+  $np = validate($_POST['np']);
+  $c_np = validate($_POST['c_np']);
+
+  if(empty($op)){
+    header("Location: modal.php?error=Current Password is required");
+    exit();
+  }else if(empty($np)){
+    header("Location: modal.php?error=New Password is required");
+    exit();
+  }else if($np !== $c_np){
+    header("Location: modal.php?error=Confirmation password does not match");
+    exit();
+  }else{
+
+    $sql = "SELECT `pass` FROM `auth` WHERE `pass` = '$op'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) === 1){
+
+        $sql_2 = "UPDATE `auth` SET `pass` = '$np' WHERE `pass` = '$op'";
+        mysqli_query($conn, $sql_2);
+        header("Location: modal.php?success=Your password has been changed successfully");
+        exit();
+
+    }else{
+      header("Location: modal.php?error=Incorrect Password");
+      exit();
+    }
+
+  }
+
+}else{
+  header("Location: modal.php?error");
+  exit();
+}
+
+//}else{
+//  header("Location: login.php");
+//  exit();
+//}
+
+?>
---
+++ modal.php
@@ -0,0 +1,44 @@
+<?php
+  //session_start();
+
+  include 'backend/conn.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inventory System</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>Change password</h1>
+      <form action = "change_pass.php" method="post">
+
+        <div class="txt_field">
+          <input type="password" name = "op" required>
+          <span></span>
+          <label>Current Password</label>
+        </div>
+
+        <div class="txt_field">
+          <input type="password" name = "np" required>
+          <span></span>
+          <label>New Password</label>
+        </div>
+
+        <div class="txt_field">
+          <input type="password" name = "c_np" required>
+          <span></span>
+          <label>Confirm New Password</label>
+        </div>
+
+        <input type="submit" value="Save">
+        <input type="submit" value="Cancel">
+
+      </form>
+    </div>
+
+  </body>
+</html>
