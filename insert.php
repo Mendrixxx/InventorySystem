5de73387706514f148170bdb9c767a9e58cd084f
epicbruhmoment69420 <umidah2@gmail.com>
2021-07-14 19:37:09 +0700

    updated logs and added some lines of codes at insert php para sa logs

--- insert.php
+++ insert.php
@@ -14,10 +14,16 @@ if(isset($_POST['add'])){
     $classif = $_POST['classification'];
     $qSO = $_POST['qSO'];
     $vSO = $_POST['vSO'];
+	
+	//Wag po tangallin yung mga lines na may comment symbol. For logs po ito.
+	$added_item = "Added Item\" " .$iname. "\"";//
+	$date = date("Y-m-d");//
 
     $additem = "INSERT into item(item_name, item_desc, property_num, date_aq, unit_meas, unit_val, total_val, quant_propcar, quant_phycou, remarks, classification, SO_quant, SO_val) values ('$iname', '$desc', '$pnum', '$dateaq', '$umeas', '$uvalue', '$tvalue', '$qPropCard', '$qPhysCount', '$remarks', '$classif', '$qSO', '$vSO')";
+	$enter_logs = "INSERT into log(item_name, action, date_action) VALUES ('$iname', '$added_item', '$date')";//
+	$add_logs = mysqli_query($db, $enter_logs);//
     $query_run = mysqli_query($db, $additem);
-    if ($query_run) {
+    if ($query_run &&  $add_logs)  {
         header("location: kapagalan.php");
     }
 }
