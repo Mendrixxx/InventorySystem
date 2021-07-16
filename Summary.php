8da0e064421699f5338a54da1d0d5ca38cbe2546
gaygayg <80329384+gaygayg@users.noreply.github.com>
2021-07-16 07:07:44 -0700

    ngudoy

--- Summary.php
+++ Summary.php
@@ -13,9 +13,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory System - Summary</title>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
@@ -27,6 +28,8 @@
     <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
         <script type="text/javascript" src="assets/js/select.js"> </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="DataTable/DataTables-1.10.25/css/jquery.dataTables.min.css">
+
 
 </head>
 
@@ -88,7 +91,7 @@
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                                 <ol class="breadcrumb">
-									<a href="#" class="btn btn-danger" >Generate PDF</a>
+                                    <a href="#" class="btn btn-danger" >Generate PDF</a>
                                 </ol>
                             </nav>
                         </div>
@@ -102,9 +105,9 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-									<div class="row">
-										<div class="col">
-																						<label>Select Classification</lable>
+                                    <div class="row">
+                                        <div class="col">
+                                                                    <label>Select Classification</lable>
 
 
 <!-- Displays on dropdown the classifications from table item-->
@@ -114,31 +117,26 @@
          <option value="1">IT</option>
          <option value="2">LABORATORY</option>
 
-	 <?php while( $rows = mysqli_fetch_array($res) ){ ?>
-	 <?php   } ?>
+       <?php while( $rows = mysqli_fetch_array($res) ){ ?>
+       <?php   } ?>
 
-	</select>
+       </select>
 
-                                            </div>
-										<div class="col-md-4">
-											<input type="text" id="first-name" class="form-control" name="fname" placeholder="Search">
-										</div>
-
-									</div>
-
+                                         </div>
+                                    </div>
                                 </div>
                                 <div class="card-content">
                                     <!-- table strip dark -->
                                     <div class="table-responsive">
-                                        <table class="table table-striped table-dark mb-0">
+                                        <table  id= "sam"  class="table table-striped table-dark mb-0">
                                             <thead>
                                                 <tr>
                                                     <th>Year</th>
                                                     <th>Total Value</th>
                                                 </tr>
-                                            </thead>
-                                            <tbody id = "ans">
+                                            </thead>      
 
+ <tbody id = "ans">
 <?php
     $sql = " SELECT YEAR(date_aq) as yearName, sum(total_val) as total from item where classification='OFFICE' group by YEAR(date_aq) order by date_aq desc ";
     $res = mysqli_query($conn,$sql);
@@ -151,15 +149,15 @@
         <td> <?php echo $rows['yearName'] ?> </td>
         <td>Php <?php echo $total ?> </td>
     </tr>
-
 <?php }?>
-
-                                        </tbody>
-                                    </table>
-                                                                <div>
-                                                                <button type="button" class="btn btn-primary" data-backdrop="static" data-toggle="modal" data-target="#rst">Save to Archive </button>
+</tbody>
+                                       </table>
+                                           
+                                     <div>
+                                                                <a href="Summary.php" class="btn btn-primary">Reset</a>
                                                               </div>
 
+                                                               
                                 </div>
                             </div>
                         </div>
@@ -183,36 +181,33 @@
 </div>
 <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
+
 <script src="assets/js/main.js"></script>
-   <!--############################################################################################################################################################################################## -->
-      <!-- DELETE component MODAL -->
-      <div class="modal fade" id="rst" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-         <div class="modal-dialog">
-            <div class="modal-content">
-               <div class="modal-header">
-                  <h4 class="modal-title custom_align" id="Heading">Notice!</h4>
-                  <button type="button" class="close" onclick="CloseModalPopup();" data-dismiss="modal" aria-hidden="true">×</button>
-               </div>
-               <form action="backend/total.php" method="POST">
-                  <div class="modal-body">
-                     <div class="alert alert-default"><span class="fa fa-exclamation-triangle"></span> Are you sure
-                        you want to archive this years inventory?</br>(This will compute for the total cost of this year.)
-                     </div>
-                  </div>
-                  <div class="modal-footer ">
-                     <button type="button" class="btn btn-secondary" onclick="CloseModalPopup();" id="cancel" data-dismiss="modal"><span
-                        class="fa fa-times-circle"></span> No</button>
-                     <button type="submit" name="reset" class="btn btn-warning" id="reset"><span
-                        class="fa fa-check-circle"></span> Yes</button>
-                  </div>
-               </form>
-            </div>
-            <!-- /.modal-content -->
-         </div>
-         <!-- /.modal-dialog -->
-      </div>
-      <!--############################################################################################################################################################################################## -->
-     
+
+
+    <!-- JQuery and DataTable Plugin-->
+   <script type = "text/javascript" src="Datatable/jquery-3.5.1.js"></script>
+   <script type = "text/javascript"  src="Datatable/DataTables-1.10.25/js/jquery.dataTables.min.js"></script>
+
+    <script type="text/javascript">
+
+    //Datatable
+    $(document).ready(function(){
+       $("#sam").DataTable({
+            "ajax":{
+                "url": "backend/summary.php",
+                "dataSrc":"",
+           },
+            "ordering": false,
+            "columns":[
+                
+                {"data":"ayear"},
+                {"data":"total"},
+            ],
+
+       });
+    });
+</script>
 </body>
 
 </html>
@@ -221,4 +216,3 @@
       header("Location: login.php");
       exit();
 }
-?>
---
+++ backend/summary.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	include "conn.php";
+
+	$sql = "SELECT * FROM archive ORDER BY ayear DESC";
+	$result = mysqli_query($conn,$sql);
+
+	$rows = array();
+	while($row = mysqli_fetch_array($result)){
+		array_push($row);
+		$rows[] = $row;
+	}
+	echo json_encode($rows);
+?>
