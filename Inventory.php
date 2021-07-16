a458527809ee8b26416a1587ae79d63ef62ded52
Calusx <86771073+Calusx@users.noreply.github.com>
2021-07-16 08:57:49 -0700

    Update Inventory.php

--- Inventory.php
+++ Inventory.php
@@ -181,8 +181,7 @@
          <section class="section">
             <div class="row" id="table-inverse">
                <div class="col-12">
-                  <div class="card">
-                     <div class="card-header">
+                  
                         <h4 class="card-title">Inventory</h4>
                      </div>
                      <div class="card-content">
