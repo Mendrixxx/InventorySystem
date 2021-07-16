d6fa55adc735bf0b81ae145aaa9dcafb30bffe2d
epicbruhmoment69420 <umidah2@gmail.com>
2021-07-16 22:59:51 +0700

    added inven per person link

--- functions/sidebar.php
+++ functions/sidebar.php
@@ -20,7 +20,7 @@
                             </a>
                         </li>
 						<li class="sidebar-item">
-                            <a href="Inventory.php" class='sidebar-link'>
+                            <a href="inventPerPerson.php" class='sidebar-link'>
                                 <i class="bi bi-grid-fill"></i>
                                 <span>Inventory Per Personnel</span>
                             </a>
