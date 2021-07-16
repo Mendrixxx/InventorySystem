77ecc9322e4449a8e3a5cac8e2d113c39aaca7d2
epicbruhmoment69420 <umidah2@gmail.com>
2021-07-16 10:33:49 +0700

    forgot something

--- backend/deletec.php
+++ backend/deletec.php
@@ -3,12 +3,12 @@
 include "conn.php";
 
 	//Wag po tangallin yung mga lines na may comment symbol. For logs po ito.
-	$delete_item = "Deleted Item";//
+	$delete_comp = "Deleted Component";//
  if (isset($_POST['continuec'])) {
         $id = $_POST['Delete_IDc'];
         $item_name = $_POST['comp_name'];
         $sql = "DELETE FROM component where comp_id = $id";
-        $del_logs = "INSERT into log(item_name, action, date_action) VALUES ('$comp_name', '$delete_item', NOW())";//
+        $del_logs = "INSERT into log(item_name, action, date_action) VALUES ('$comp_name', '$delete_comp', NOW())";//
 
         $run = mysqli_query($conn, $sql);
         $log_del = mysqli_query($conn, $del_logs);//
