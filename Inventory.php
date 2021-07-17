dbd16a20c4513cb66f838d766c4025c97bc76c2c
Calusx <86771073+Calusx@users.noreply.github.com>
2021-07-16 19:58:36 -0700

    Update Inventory.php

--- Inventory.php
+++ Inventory.php
@@ -18,13 +18,13 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
       <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="assets/css/bg.css">  
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet"> 
       <link rel="stylesheet" href="assets/css/bootstrap.css">
       <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
       <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
       <link rel="stylesheet" href="assets/css/app.css">
       <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon"  >
+<link rel="stylesheet" href="assets/css/bg.css"> 
 	  
       <script src="assets/js/jquery-3.6.0.min.js"></script>
       <link rel="stylesheet" type="text/css" href="DataTable/datatables.min.css">
