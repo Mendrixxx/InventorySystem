bdaf364b458f9a1eb1baa52d44e091afa514d9b9
Prinsipeyyy <87378692+Prinsipeyyy@users.noreply.github.com>
2021-07-25 15:26:22 +0800

    Add files via upload

---
+++ try.php
@@ -0,0 +1,37 @@
+<?php
+   
+  
+   include './backend/conn.php';
+
+   if ($conn->connect_error) {
+       die("Connection Failed: ". $conn->connect_error);
+   } else {
+       $items = getData($conn);
+     //  $summary = getSummary($items);
+       //krsort($summary);
+       // print_r($summary);
+       //echo json_encode($summary);
+   }
+
+     // functions
+   function getData($conn) {
+       $query = " SELECT
+       *
+   FROM
+       item AS i
+       LEFT JOIN employee AS e ON (i.remarks = e.id)
+   LEFT JOIN component AS c ON (i.item_id=c.item_id) order by i.item_id asc";
+       $result = mysqli_query($conn, $query);
+       $dataArr = getRowsFrmDB($result);
+       return $dataArr;
+   };
+
+   function getRowsFrmDB($result) {
+       if (!empty($result)) {
+           return mysqli_fetch_all($result, MYSQLI_ASSOC);
+       } else {
+           return [];
+       }
+   }
+     
+?>
