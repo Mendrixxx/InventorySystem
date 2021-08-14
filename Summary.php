949c6a0dd03deb2dfd589d278f02e68bd427e100
epicbruhmoment69420 <umidah2@gmail.com>
2021-08-14 21:46:05 +0700

    removed the duplicate burger icon in summary

--- Summary.php
+++ Summary.php
@@ -58,12 +58,6 @@
    <body>
     <!--Sidebars-->
     <?php require_once "functions/sidebar.php" ?>
-  
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
 
             <div class="page-heading">
                 <div class="page-title">
